0beddf0ef5e8302591796646cb7c8e4d6aae4be1
Paul Debevec <paulmdebevec@gmail.com>
2020-01-09 16:28:26 -0700

    Updated users schema to include an active? column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200108051959) do
+ActiveRecord::Schema.define(version: 20200109213817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20200108051959) do
     t.string "password_digest"
     t.integer "role", default: 0
     t.bigint "merchant_id"
+    t.boolean "active?", default: true
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
