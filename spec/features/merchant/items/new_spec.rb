d220682416bc59f9e3b3ccd3f07994796a3c2772
Paul Debevec <paulmdebevec@gmail.com>
2020-01-09 19:20:16 -0700

    Add test item creation will not save with incomplete form or invalid quantities
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- spec/features/merchant/items/new_spec.rb
+++ spec/features/merchant/items/new_spec.rb
@@ -52,6 +52,7 @@ RSpec.describe "Create Merchant Items" do
       expect(Item.last.active?).to be(true)
       expect("#item-#{Item.last.id}").to be_present
       expect(page).to have_content(name)
+      expect(page).to have_content("Item Created!")
     end
 
     it 'I get an alert if I dont fully fill out the form' do
@@ -73,8 +74,60 @@ RSpec.describe "Create Merchant Items" do
 
       click_button "Create Item"
 
-      expect(page).to have_content("Name can't be blank and Inventory can't be blank")
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Inventory can't be blank")
       expect(page).to have_button("Create Item")
+    end
+
+    it 'will not save if inventory is lower than 0, and I get a flash message' do
+      visit "/merchant/items"
+
+      name = "Bobby Ray"
+      price = 18
+      description = "No more chaffin'!"
+      image_url = "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg"
+      inventory = -4
+
+      click_on "Add Item"
+
+      fill_in :name, with: name
+      fill_in :price, with: price
+      fill_in :description, with: description
+      fill_in :image, with: image_url
+      fill_in :inventory, with: inventory
+
+      click_button "Create Item"
+
+      expect(page).to have_content("Inventory must be greater than -1")
+      expect(page).to have_button("Create Item")
+    end
+
+    it 'When I save an incomplete form the fields remain populated' do
+      visit "/merchant/items"
+
+      name = "Chamois Buttr"
+      price = 5
+      # description = "No more chaffin'!"
+      image_url = "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg"
+      inventory = 25
+
+      click_on "Add Item"
+
+      fill_in :name, with: name
+      fill_in :price, with: price
+      # fill_in :description, with: description
+      fill_in :image, with: image_url
+      fill_in :inventory, with: inventory
+
+      click_button "Create Item"
+
+      incomplete_item = Item.last
+
+      find_field(:name, with: "Chamois Buttr")
+      find_field(:price, with: 5)
+      find_field(:description, with: "")
+      find_field(:image, with: "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg")
+      find_field(:inventory, with: 25)
     end
   end
 end
